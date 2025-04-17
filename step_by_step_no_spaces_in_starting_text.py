5ede4b67154f0aecda957735abfbfb9308ebc013
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-17 18:50:44 +0800

    Added a variable for the input for text

--- step_by_step_no_spaces_in_starting_text.py
+++ step_by_step_no_spaces_in_starting_text.py
@@ -3,3 +3,5 @@ def no_spaces_in_starting_text(text):
     while starting_spaces < len(text) and text[starting_spaces] == " ":
         starting_spaces += 1
     return text[starting_spaces:]
+
+text = input("Enter text: ")
