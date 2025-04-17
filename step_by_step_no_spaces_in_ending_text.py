9eb51e0b2dde2b37ee5ebaf29c68ec6a0adda5be
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-17 19:13:41 +0800

    Return the result of the variable for the spaces in the end of the text decreased by 1 that is sliced in the end as an index for the text

--- step_by_step_no_spaces_in_ending_text.py
+++ step_by_step_no_spaces_in_ending_text.py
@@ -2,3 +2,4 @@ def no_spaces_in_ending_text(text):
     ending_spaces = len(text) - 1
     while ending_spaces >= 0 and text[ending_spaces] == " ":
         ending_spaces -= 1
+    return text[:ending_spaces + 1]
