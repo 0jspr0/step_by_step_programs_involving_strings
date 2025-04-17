40d04b045a713111131edb15b82725ba0a9f478d
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-17 18:47:52 +0800

    Return the result of the variable for the spaces in the start of the text that is sliced in the start as an index for the text

--- step_by_step_no_spaces_in_starting_text.py
+++ step_by_step_no_spaces_in_starting_text.py
@@ -2,3 +2,4 @@ def no_spaces_in_starting_text(text):
     starting_spaces = 0
     while starting_spaces < len(text) and text[starting_spaces] == " ":
         starting_spaces += 1
+    return text[starting_spaces:]
