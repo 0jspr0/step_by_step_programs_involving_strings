220474a1fe9bed244adc33cbac2537886dddccb2
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-20 23:30:19 +0800

    Added a variable for the input for text

--- step_by_step_matching_starting_text_checker.py
+++ step_by_step_matching_starting_text_checker.py
@@ -2,3 +2,5 @@ def matching_starting_text_checker(text, starting_text):
     if len(starting_text) > len(text):
         return False
     return text[:len(starting_text)] == starting_text
+
+text = input("Enter text: ")
