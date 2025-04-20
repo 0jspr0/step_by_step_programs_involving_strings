f9209d8bd5d69be0ae617b875cb29c14e13fc06a
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-20 23:26:05 +0800

    Added a variable for the input for text

--- step_by_step_matching_ending_text_checker.py
+++ step_by_step_matching_ending_text_checker.py
@@ -2,3 +2,5 @@ def matching_ending_text_checker(text, ending_text):
     if len(ending_text) > len(text):
         return False
     return text[-len(ending_text):] == ending_text
+
+text = input("Enter text: ")
