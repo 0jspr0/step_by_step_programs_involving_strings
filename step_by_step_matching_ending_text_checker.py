8f2c0e1b221c32d81a4e534268caddee41ccdb08
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-20 23:27:29 +0800

    Display the result

--- step_by_step_matching_ending_text_checker.py
+++ step_by_step_matching_ending_text_checker.py
@@ -6,3 +6,4 @@ def matching_ending_text_checker(text, ending_text):
 text = input("Enter text: ")
 ending_text = input("Enter ending text: ")
 result = matching_ending_text_checker(text, ending_text)
+print(result)
