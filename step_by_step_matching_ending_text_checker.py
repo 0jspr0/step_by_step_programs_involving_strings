1a2bf2114db9d83286313677457f41a3fea67230
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-20 23:27:02 +0800

    Added a variable for the result of calling the created function with the variables for the input for text and text to remove as the parameters

--- step_by_step_matching_ending_text_checker.py
+++ step_by_step_matching_ending_text_checker.py
@@ -5,3 +5,4 @@ def matching_ending_text_checker(text, ending_text):
 
 text = input("Enter text: ")
 ending_text = input("Enter ending text: ")
+result = matching_ending_text_checker(text, ending_text)
