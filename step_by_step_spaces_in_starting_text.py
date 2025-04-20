53735e60d8772930f3de4b62533516e21328eae6
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-20 23:43:47 +0800

    Return the text with the spaces in the start

--- step_by_step_spaces_in_starting_text.py
+++ step_by_step_spaces_in_starting_text.py
@@ -2,3 +2,4 @@ def spaces_in_starting_text(text, number_of_spaces):
     if len(text) >= number_of_spaces:
         return text
     number_of_spaces -= len(text)
+    return " " * number_of_spaces + text
