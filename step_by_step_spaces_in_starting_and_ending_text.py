86b060deb9e83c124642a607b0ed32606b7b667e
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-21 23:27:44 +0800

    Set the number of the starting spaces to itself decreased by the number of the ending spaces

--- step_by_step_spaces_in_starting_and_ending_text.py
+++ step_by_step_spaces_in_starting_and_ending_text.py
@@ -2,3 +2,4 @@ def spaces_in_starting_and_ending_text(text, number_of_spaces):
     if len(text) >= number_of_spaces:
         return text
     number_of_ending_spaces = number_of_spaces // 2
+    number_of_starting_spaces = number_of_spaces - number_of_ending_spaces
