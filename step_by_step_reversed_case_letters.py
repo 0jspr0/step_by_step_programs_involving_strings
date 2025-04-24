616c3d43dc6d2a482a023565dd2ccd9b304af4a6
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-24 23:36:41 +0800

    If a letter is a lowercase then turn it into uppercase otherwise if a letter is an uppercase them turn it into lowercase otherwise keep it

--- step_by_step_reversed_case_letters.py
+++ step_by_step_reversed_case_letters.py
@@ -1,2 +1,9 @@
 def reversed_case_letters(text):
     result = ""
+    for i in text:
+        if "a" <= i <= "z":
+            result += chr(ord(i) - 32)
+        elif "A" <= i <= "Z":
+            result += chr(ord(i) + 32)
+        else:
+            result += i
