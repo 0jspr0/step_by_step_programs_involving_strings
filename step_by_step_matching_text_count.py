81063ba3727f3d9b6a33de2e62b92f6baad083b4
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-04-26 23:08:22 +0800

    Display the result

--- step_by_step_matching_text_count.py
+++ step_by_step_matching_text_count.py
@@ -12,3 +12,4 @@ def matching_text_count(text, text_to_count):
 text = input("Enter text: ")
 text_to_count = input("Enter text to count: ")
 result = matching_text_count(text, text_to_count)
+print(result)
